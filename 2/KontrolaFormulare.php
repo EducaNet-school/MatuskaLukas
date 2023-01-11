795703d806bdc10a93b054efb78a45bb5f9d8a54
TriZyeR <yobachu@gmail.com>
2023-01-11 10:08:30 +0100

    Create KontrolaFormulare.php

---
+++ 2/KontrolaFormulare.php
@@ -0,0 +1,37 @@
+<form method="post">
+  <h1>Kontrola formuláře</h1>
+  <label for="name">Jméno a příjmení:</label>
+  <input type="text" name="name">
+  <br>
+  <label for="address">Adresa:</label>
+  <input type="text" name="address">
+  <br>
+  <input type="submit" value="Odeslat" name="submit">
+</form>
+
+<?php
+function validateForm() {
+    if (isset($_POST['name']) && isset($_POST['address'])) {
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+
+        if (empty($name)) {
+            echo "<p style='color: red;'>Jméno a příjmení musí být vyplněno!</p>";
+        } else if(count(explode(" ", $name)) < 2) {
+            echo "<p style='color: red;'>Zadejte prosím jméno i příjmení</p>";
+        } 
+
+        else if (strlen($address) < 10) {
+            echo "<p style='color: red;'>Adresa musí obsahovat alespoň 10 znaků!</p>";
+        } 
+        else {
+            echo "Jméno: " . $name . "<br>";
+            echo "Adresa: " . $address;
+        }
+    }
+}
+
+if(isset($_POST["submit"])){
+    validateForm();
+}
+?>
